2232b0f87d0c748f25e8e317c11a3449fb5128ef
LeeSangmin <sanggusbab@gmail.com>
2022-09-26 17:19:18 +0900

    First MVVM multimodule structure Example

--- app/src/main/java/com/example/kat/model/DataModel.kt
+++ app/src/main/java/com/example/kat/model/DataModel.kt
@@ -1,4 +1,6 @@
 package com.example.kat.model
 
 // Contains the data we want to show on UI (in MainFragment)
-data class DataModel(val textForUI: String)
+data class DataModel(val textForUI: String){
+    val i : Int = 3
+}
