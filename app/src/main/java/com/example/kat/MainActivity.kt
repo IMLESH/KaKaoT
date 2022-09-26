871135b7e1d41d9df1efd5bfb3c42ddfb9853635
LeeSangmin <sanggusbab@gmail.com>
2022-09-26 13:27:33 +0900

    MVVM Structure

--- app/src/main/java/com/example/kat/MainActivity.kt
+++ app/src/main/java/com/example/kat/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.kat
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    private var i : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +17,14 @@ class MainActivity : Activity() {
         val btn_hello:Button = findViewById<Button>(R.id.btn_hello)
 
         btn_hello.setOnClickListener() {
-            Toast.makeText(this, "토스트메시지22",Toast.LENGTH_SHORT).show()
+            i++
+            Toast.makeText(this, "토스트메시지22"+ i,Toast.LENGTH_SHORT).show()
+            if(i >= 3)
+            {
+                i = 0
+                val nextIntent = Intent(this, com.example.kat.view.MainActivity::class.java)
+                startActivity(nextIntent)
+            }
         }
         //내용 작성하기
     }
---
+++ app/src/main/java/com/example/kat/view/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kat.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.kat.R
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        if(savedInstanceState ==null){
+            supportFragmentManager.commit{
+                setReorderingAllowed(true)
+                add<MainFragment>(R.id.fragment_container_view)
+            }
+        }
+    }
+}
